e57d6a90f342ab84742cdde6f2154acc575b4a52
sarahxychen <sarahxy.chen@mail.utoronto.ca>
2024-03-30 17:19:04 -0400

    framework comments for data classification

--- datamap.js
+++ datamap.js
@@ -17,7 +17,10 @@ const map2 = new mapboxgl.Map({
 map2.addControl(new mapboxgl.NavigationControl());
 map2.addControl(new mapboxgl.FullscreenControl());
 
+
+/*--------------------------------------------------------------------
 //View GeoJSON data on map as interactive layer: CMHC Data
+--------------------------------------------------------------------*/
 
 let cmhcgeojson;
 
@@ -46,10 +49,53 @@ map2.on('load', () => {
           'fill-outline-color': 'blue'
         }
     });
+
+    //2006 Housing Standard (Housing_st)
+
+    map2.addLayer({
+        'id': '2006_housingst',
+        'type': 'fill',
+        'source': 'cmhc_data',
+        'paint': {
+
+    // refer to legend in experience builder to classify each layer
+    //         'circle-radius': [
+    //             'interpolate', //INTERPOLATE expression produces continuous results by interpolating between value pairs
+    //             ['linear'], //linear interpolation between stops but could be exponential ['exponential', base] where base controls rate at which output increases
+    //             ['zoom'], //zoom expression changes appearance with zoom level
+    //             10, 7, // when zoom is 10 (or less), radius will be 7px
+    //             12, 5 // when zoom is 12 (or greater), radius will 5px
+    //        ],
+    //    'circle-color': [
+    //        'step', // STEP expression produces stepped results based on value pairs //Classify biking points based on type of bike parking capacity in column: Bicycle_capacity to display ramp colour on points
+    //        ['get', 'BICYCLE_CAPACITY'], // GET expression retrieves property value from 'capacity' data field
+    //        '#f07dcf', // Colour assigned to any values < first step (so 0-9)
+    //        10, '#d10496', // Colours assigned to values >= each step (10-19)
+    //        20, '#d9027c', // >=(20-29)
+    //        30, '#9c0259', // >=(30-119)
+    //        120, '#52022f', // >=(120)
+        }
+    });
     
 });
 
+//View and classify variable layers
+
+    //2006 Housing Standard (Housing_st)
+
+    //2011 Housing Standard (Housing__1)
+
+    //2016 Housing Standard (Housing__2)
+
+    //2006 Property Value (Value_dwel)
+
+    //2011 Property Value (Value_dw_1)
+
+    //2016 Property Value (Value_dw_2)
+
+/*--------------------------------------------------------------------
 //View GeoJSON data on map as interactive layer: Census data
+--------------------------------------------------------------------*/
 
 let censusgeojson;
 
