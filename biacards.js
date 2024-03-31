f4922a0378d5a4cc4a80ffd387a7093b72556697
charlbkg <156369623+charlbkg@users.noreply.github.com>
2024-03-31 17:17:34 -0400

    Updating timeslider

--- biacards.js
+++ biacards.js
@@ -5,14 +5,14 @@ function search() {
     filter = input.value.toUpperCase();
     cards = document.getElementsByClassName("card")
     titles = document.getElementsByClassName("card-title");
-  
+
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < cards.length; i++) {
-      a = titles[i];
-      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        cards[i].style.display = "";
-      } else {
-        cards[i].style.display = "none";
-      }
+        a = titles[i];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
     }
-  }
+}
