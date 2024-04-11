bdc1f0f85510278b9a394a6a6b9a8d1d4edc2a62
charlbkg <156369623+charlbkg@users.noreply.github.com>
2024-04-10 20:15:19 -0400

    update

--- timemap.js
+++ timemap.js
@@ -5,83 +5,13 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic2FyYWh4eWNoZW4iLCJhIjoiY2xyZnB4c2h0MDhnMzJqc
 const map1 = new mapboxgl.Map({
     container: 'timemap', // container id in HTML
     style: 'mapbox://styles/sarahxychen/clskmpfs603tf01p25v25bs4j',  //change if we want
-    center: [-79.39, 43.65],  // starting point, longitude/latitude
-    zoom: 12 // starting zoom level
+    center: [-79.42, 43.69],  // starting point, longitude/latitude
+    zoom: 10.75 // starting zoom level
 });
 
 // Add zoom and rotation controls to the map.
 map1.addControl(new mapboxgl.NavigationControl(), 'bottom-left');
 map1.addControl(new mapboxgl.FullscreenControl(), 'bottom-left');
-
-//Add timeslider functionality
-
-// const years = [
-//     "1969-01-01",
-//     "1970-01-01",
-//     "1971-01-01",
-//     "1972-01-01",
-//     "1973-01-01",
-//     "1974-01-01",
-//     "1975-01-01",
-//     "1976-01-01",
-//     "1977-01-01",
-//     "1978-01-01",
-//     "1979-01-01",
-//     "1980-01-01",
-//     "1981-01-01",
-//     "1982-01-01",
-//     "1983-01-01",
-//     "1984-01-01",
-//     "1985-01-01",
-//     "1986-01-01",
-//     "1987-01-01",
-//     "1988-01-01",
-//     "1989-01-01",
-//     "1990-01-01",
-//     "1991-01-01",
-//     "1992-01-01",
-//     "1993-01-01",
-//     "1994-01-01",
-//     "1995-01-01",
-//     "1996-01-01",
-//     "1997-01-01",
-//     "1998-01-01",
-//     "1999-01-01",
-//     "2000-01-01",
-//     "2001-01-01",
-//     "2002-01-01",
-//     "2003-01-01",
-//     "2004-01-01",
-//     "2005-01-01",
-//     "2006-01-01",
-//     "2007-01-01",
-//     "2008-01-01",
-//     "2009-01-01",
-//     "2010-01-01",
-//     "2011-01-01",
-//     "2012-01-01",
-//     "2013-01-01",
-//     "2014-01-01",
-//     "2015-01-01",
-//     "2016-01-01",
-//     "2017-01-01",
-//     "2018-01-01",
-//     "2019-01-01",
-//     "2020-01-01",
-//     "2021-01-01"
-// ];
-// function filterBy(year) {
-//     const filters = ['==', 'years', year];
-//     map1.setFilter('biapoly', filters);
-
-//     // Set the label to the month
-//     document.getElementById('years').textContent = years[year];
-// };
-
-// function jsonCallback(err, data) {
-//     if (err) {
-//         throw err;
-//     }
 
 map1.on('load', () => {
     map1.addSource('biapoly', {
@@ -100,6 +30,48 @@ map1.on('load', () => {
         },
     });
 });
+        // When a click event occurs on a feature in the places layer, open a popup at the
+        // location of the feature, with description HTML from its properties.
+        map1.on('click', 'biapoly', (e) => {
+            // Copy coordinates array.
+            const coordinates = e.features[0].geometry.coordinates.slice();
+            const description = e.features[0].properties.Descriptio;
+
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            new mapboxgl.Popup()
+                .setLngLat(coordinates)
+                .setHTML(description)
+                .addTo(map1);
+        });
+
+        // Change the cursor to a pointer when the mouse is over the places layer.
+        map1.on('mouseenter', 'places', () => {
+            map1.getCanvas().style.cursor = 'pointer';
+        });
+
+        // Change it back to a pointer when it leaves.
+        map1.on('mouseleave', 'places', () => {
+            map1.getCanvas().style.cursor = '';
+        });
+  
+
+
+// map1.on('click', e => {
+//     const result = map1.queryRenderedFeatures(e.point, { layers:['biapoly'] });
+//     if (result.length) {
+//     new mapboxgl.Popup()
+//     .setLngLat(e.lngLat)
+//     .setHTML('<h1>hi</h1>')
+//     .addTo(map1);
+//     }
+// });
+
 document.getElementById('slider').addEventListener('input', (event) => {
     const year = parseInt(event.target.value);
     // update the map
@@ -110,24 +82,24 @@ document.getElementById('slider').addEventListener('input', (event) => {
 // When a click event occurs on a feature in the bia layer,
 // open a popup at the location of the click, with description
 // HTML from the click event's properties.
-map1.on('click', 'biapoly', (e) => {
-    new mapboxgl.Popup()
-        .setLngLat(e.lngLat)
-        .setHTML(description)
-        .addTo(map1);
-});
+// map1.on('click', 'biapoly', (e) => {
+//     new mapboxgl.Popup()
+//         .setLngLat(e.lngLat)
+//         .setHTML('<h1>heyo</h1>')
+//         .addTo(map1);
+// });
 
-// Change the cursor to a pointer when
-// the mouse is over the states layer.
-map1.on('mouseenter', 'biapoly', () => {
-    map1.getCanvas().style.cursor = 'pointer';
-});
+// // Change the cursor to a pointer when
+// // the mouse is over the states layer.
+// map1.on('mouseenter', 'biapoly', () => {
+//     map1.getCanvas().style.cursor = 'pointer';
+// });
 
-// Change the cursor back to a pointer
-// when it leaves the BIA polygon layer.
-map1.on('mouseleave', 'biapoly', () => {
-    map1.getCanvas().style.cursor = '';
-});
+// // Change the cursor back to a pointer
+// // when it leaves the BIA polygon layer.
+// map1.on('mouseleave', 'biapoly', () => {
+//     map1.getCanvas().style.cursor = '';
+// });
 
 
 
